93e8079b9e8cd3ef7f12b465057611ee3878a83c
Snaptraks <snaptraks@gmail.com>
2021-04-20 13:47:41 -0400

    Make yarr command trigger a menu

--- cogs/SeaofThieves/seaofthieves.py
+++ cogs/SeaofThieves/seaofthieves.py
@@ -1,16 +1,8 @@
 from collections import defaultdict
-import itertools
-import json
-import os
-import random
 
 import discord
 from discord.ext import commands
-from ..utils.gifs import random_gif
-
-
-COG_PATH = os.path.dirname(__file__)
-EMBED_COLOR = 0x2C8C77
+from . import menus
 
 
 class SeaofThieves(commands.Cog):
@@ -20,37 +12,18 @@ class SeaofThieves(commands.Cog):
         self.bot = bot
         self.pirate_role = defaultdict(lambda: None)
 
-        with open(os.path.join(COG_PATH, "chat_wheel.json")) as f:
-            self.chat_wheel = json.load(f)
-
     @commands.command(aliases=["yar"])
-    async def yarr(self, ctx):
+    async def yarr(self, ctx, crew: int = 1):
         """Gather mateys to sail the Sea of Thieves!"""
 
         if self.pirate_role[ctx.guild.id] is None:
-            self.pirate_role[ctx.guild.id] = discord.utils.get(
+            role = discord.utils.get(
                 ctx.guild.roles, name="Pirate")
-
-        all_chat = itertools.chain.from_iterable(self.chat_wheel.values())
-        valid_ends = (".", "!", "?", "*")
-        valid_chats = set([x for x in all_chat if x.endswith(valid_ends)])
-        if ctx.author.id == 242465567259230208:  # Elerinna
-            random_chat = f":rainbow: {self.chat_wheel['alerts'][-1]}"
-        else:
-            random_chat = random.choice(list(valid_chats))
-
-        description = (
-            f"**{random_chat}**\n{ctx.author.mention} sails today, "
-            "join in on the plunder!"
+            if role is None:
+                role = ctx.guild.default_role
+            self.pirate_role[ctx.guild.id] = role
+        menu = menus.YarrMenu(
+            timeout=None,
+            pirate_role=self.pirate_role[ctx.guild.id],
         )
-        gif_url = await random_gif(self.bot.http_session, "pirate")
-
-        embed = discord.Embed(
-            description=description,
-            color=EMBED_COLOR,
-        ).set_image(
-            url=gif_url,
-        )
-
-        await ctx.send(embed=embed)
-        await ctx.send(self.pirate_role[ctx.guild.id].mention, embed=embed)
+        await menu.start(ctx)
