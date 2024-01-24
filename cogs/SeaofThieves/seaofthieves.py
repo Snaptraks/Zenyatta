b27d6c31fbcd602b12fe34d4f9ef111c056445a9
Snaptraks <snaptraks@gmail.com>
2024-01-24 11:59:47 -0500

    Improve yarr command

--- cogs/SeaofThieves/seaofthieves.py
+++ cogs/SeaofThieves/seaofthieves.py
@@ -11,26 +11,22 @@ from ..utils.gifs import random_gif
 class SeaofThieves(commands.Cog):
     """Commands for Sea of Thieves related activities."""
 
-    def __init__(self, bot):
+    def __init__(self, bot: Bot):
         self.bot = bot
-        self.pirate_role = defaultdict(lambda: None)
+        LOGGER.debug("Starting Gold Rush")
+        self.gold_rush.start()
 
     @commands.hybrid_command(aliases=["yar"])
     @app_commands.describe(message="What's the plan, captain?")
     async def yarr(self, ctx, *, message: str = ""):
         """Gather mateys to sail the Sea of Thieves!"""
 
-        guild_id = ctx.guild.id
-        if self.pirate_role[guild_id] is None:
-            role = discord.utils.get(ctx.guild.roles, name="Pirate")
-            if role is None:
-                role = ctx.guild.default_role
-            self.pirate_role[guild_id] = role
+        role = ctx.guild.get_role(PIRATE_ROLE)
+        if role is None:
+            role = ctx.guild.default_role
 
         gif_url = await random_gif(self.bot.http_session, "sea of thieves")
         view = views.YarrView(ctx.author)
         embed = view.build_embed(gif_url)
 
-        await ctx.send(
-            f"{self.pirate_role[guild_id].mention}\n{message}", embed=embed, view=view
-        )
+        await ctx.send(f"{role.mention}\n{message}", embed=embed, view=view)
